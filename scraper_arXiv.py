dc669232d621463a547351b99fd84eb405484cdf
juamarhe2002 <juanmaristizabal1.0@gmail.com>
2024-12-17 00:38:37 +0100

    Scraper sorting algorithm by popularity. Initial implementation.

--- scraper_arXiv.py
+++ scraper_arXiv.py
@@ -1,15 +1,14 @@
-from typing import List
-
 import requests
 from bs4 import BeautifulSoup, PageElement
+import re
 import pandas as pd
 import sqlite3
-from apscheduler.schedulers.blocking import BlockingScheduler
 
 
 class ScraperArXiv:
     def __init__(self, url: str):
         self.url = url
+        self.articles = []
 
         self.conn = sqlite3.connect('articles.db')
         self.cur = self.conn.cursor()
@@ -24,30 +23,81 @@ class ScraperArXiv:
         return True if result is None else False
 
     # Function to save new articles to the database
-    def save_article(self, article):
-        self.cur.execute('INSERT INTO Articles (arXiv_id, title, authors, subjects, pdf_link) VALUES (?, ?, ?, ?, ?)',
-                         (article['arXiv_id'], article['title'], article['authors'], article['subjects'],
-                          article['pdf_link']))
-        self.conn.commit()
+    def save_articles(self, articles: list):
+        for article in articles:
+            self.cur.execute('''INSERT INTO Articles (arXiv_id, title, authors, subjects, comments, popularity, pdf_link)
+                             VALUES (?, ?, ?, ?, ?, ?, ?)''',
+                             (article['arXiv_id'], article['title'], article['authors'], article['subjects'],
+                              article['comments'], article['popularity'], article['pdf_link']))
+            self.conn.commit()
 
+    # Function to check if the article was noted by arXiv admin with text overlap.
     @staticmethod
-    def extract_article_data(item: PageElement) -> dict:
+    def has_text_overlap(article_comments: dict) -> bool:
+        comments = article_comments['comments']
+        return True if 'arxiv admin note: substantial text overlap' in comments else False
+
+    # Function to calculate the popularity of an article.
+    @staticmethod
+    def calculate_popularity(journal_ref, comments_text: str) -> int:
+        if journal_ref is not None:
+            return 2
+
+        if len(comments_text) > 0:
+            match = re.search(r"([tT]o be ){0}(([aA]ccepted)|([pP]ublished))\s", comments_text)
+            if match is not None:
+                return 1
+
+        return 0
+
+    # Function to extract the relevant data from an article.
+    def extract_article_data(self, item: PageElement) -> dict:
         title_tag = item.find_previous_sibling('dt')
         arXiv_id = title_tag.find('a', title='Abstract')['id']
-        title = item.find('div', class_='list-title').get_text(strip=True).replace('Title:', '').strip()
+        title = item.find('div', class_='list-title').get_text(strip=True).replace('Title:', '')
         authors = item.find('div', class_='list-authors').get_text(strip=True)
-        subjects = item.find('div', class_='list-subjects').get_text(strip=True).replace('Subjects:', '').strip()
+        subjects = item.find('div', class_='list-subjects').get_text(strip=True).replace('Subjects:', '')
         pdf_link = "https://arxiv.org" + title_tag.find('a', title='Download PDF')['href']
+
+        journal_ref = item.find('div', class_='list-journal-ref')
+        comments = item.find('div', 'list-comments')
+        comments_text = comments.get_text(strip=True) if comments is not None else ''
+
+        popularity = self.calculate_popularity(journal_ref, comments_text)
 
         article_data = {
             'arXiv_id': arXiv_id,
             'title': title,
             'authors': authors,
             'subjects': subjects,
+            'comments': comments_text,
+            'popularity': popularity,
             'pdf_link': pdf_link
         }
 
         return article_data
+
+    # Function to save the subjects of the articles.
+    @staticmethod
+    def save_articles_subjects(subjects: dict, new_article: dict):
+        new_subjects = new_article['subjects'].split(';')
+
+        for new_subject in new_subjects:
+            if new_subject not in subjects:
+                subjects[new_subject] = [[new_article['arXiv_id'], new_article['popularity']]]
+            else:
+                subjects[new_subject].append([new_article['arXiv_id'], new_article['popularity']])
+
+    # Function for checking the number of articles of each subject and returning the
+    # subject that has an equivalent number of articles as the one required.
+    # returns: subject name (str) | (None)
+    @staticmethod
+    def chk_articles_number(subjects: dict, num_articles: int) -> str | None:
+        for subject in subjects:
+            if len(subjects[subject]) == num_articles:
+                return subject
+
+        return None
 
     # Function to fetch and parse HTML content from arXiv
     def fetch_articles(self, num_articles: int) -> list[dict] | None:
@@ -58,26 +108,27 @@ class ScraperArXiv:
             soup = BeautifulSoup(response.text, 'html.parser')  # Parse HTML content
 
             # Extract metadata for each article
+            subjects = dict()
             articles = []
             items = soup.find_all('dd')  # Content of articles is in <dd> tags
             for item in items:
                 new_article = self.extract_article_data(item)
 
-                if self.is_new_article(new_article['arXiv_id']):
-                    articles.append(new_article)
-                    self.save_article(new_article)  # Save the new article to the database
+                if self.is_new_article(new_article['arXiv_id']) and not self.has_text_overlap(new_article):
+                    if new_article['popularity'] == 2:
+                        articles.append(new_article)
 
-                # Stop collecting if we have the number of articles expected to have.
-                if len(articles) == num_articles:
-                    break
+                    if len(articles) == num_articles:
+                        self.save_articles(articles)
+                        self.articles = articles
+                        break
 
             print("Scraper: Fetched new articles successfully!")
-            return articles
+            return self.articles
 
         except requests.RequestException as e:
-            print(f"Error fetching data from arXiv: {e}")
+            print(f"Error fetching data from url {self.url}: {e}")
             return None
         except Exception as e:
             print(f"Error parsing data: {e}")
             return None
-
--- test_scraper_arXiv.py
+++ test_scraper_arXiv.py
@@ -19,6 +19,8 @@ class ScraperTestCase(unittest.TestCase, ScraperArXiv):
                     title TEXT UNIQUE NOT NULL,
                     authors TEXT NOT NULL,
                     subjects TEXT NOT NULL,
+                    comments TEXT,
+                    popularity INTEGER DEFAULT 0,
                     pdf_link TEXT NOT NULL);
                 ''')
         self.conn.commit()
@@ -30,27 +32,50 @@ class ScraperTestCase(unittest.TestCase, ScraperArXiv):
     def test_initialize_database(self):
         self.cur.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='Articles';")
         result = self.cur.fetchone()
+        self.assertEqual(('Articles',), result, "Assert: test_initialize_database failed in table creation\n")
 
-        self.assertEqual(('Articles',), result, "Assert: test_initialize_database failed\n")
+        self.cur.execute("PRAGMA table_info(Articles);")
+        result = self.cur.fetchall()
+        columns = ['id', 'arXiv_id', 'title', 'authors', 'subjects', 'comments', 'popularity', 'pdf_link']
+        result = [col_info[1] for col_info in result]
+        self.assertEqual(columns, result, "Assert: test_initialize_database failed in schema creation.\n")
 
     # Function to test saving and retrieving articles
     def test_article_functions(self):
-        test_article = {
-            'arXiv_id': 'arXiv_id',
-            'title': 'title',
+        test_article_1 = {
+            'arXiv_id': 'arXiv_id_1',
+            'title': 'title_1',
             'authors': 'authors',
             'subjects': 'subjects',
+            'comments': 'comments',
+            'popularity': 0,
+            'pdf_link': 'pdf_link'
+        }
+
+        test_article_2 = {
+            'arXiv_id': 'arXiv_id_2',
+            'title': 'title_2',
+            'authors': 'authors',
+            'subjects': 'subjects',
+            'comments': 'comments',
+            'popularity': 2,
             'pdf_link': 'pdf_link'
         }
 
         # Check if the article is new (should be True initially)
-        is_new_before = self.is_new_article(test_article['arXiv_id'])
-        self.assertEqual(is_new_before, True, "Assert: is_new_article() attempt 1 failed\n")
+        is_new_before = self.is_new_article(test_article_1['arXiv_id'])
+        self.assertEqual(is_new_before, True, "Assert: is_new_article(test_article_1) attempt 1 failed\n")
+        is_new_before = self.is_new_article(test_article_2['arXiv_id'])
+        self.assertEqual(is_new_before, True, "Assert: is_new_article(test_article_2) attempt 1 failed\n")
 
         # Save the article and check again (should be False)
-        self.save_article(test_article)
-        is_new_after = self.is_new_article(test_article['arXiv_id'])
-        self.assertEqual(is_new_after, False, "Assert: is_new_article() attempt 2 failed\n")
+        test_articles = [test_article_1, test_article_2]
+        self.save_articles(test_articles)
+
+        is_new_after = self.is_new_article(test_article_1['arXiv_id'])
+        self.assertEqual(is_new_after, False, "Assert: is_new_article(test_article_1) attempt 2 failed\n")
+        is_new_after = self.is_new_article(test_article_2['arXiv_id'])
+        self.assertEqual(is_new_after, False, "Assert: is_new_article(test_article_2) attempt 2 failed\n")
 
     # Function to test if the articles are fetched correctly.
     def test_fetch_articles(self):
